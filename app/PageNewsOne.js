69b46148aa0f3c3e8e22ec89eced073e331988e5
KateMas <e.masyuta@gmail.com>
2017-01-13 18:02:54 +0200

    added button, title and body from one post-json-placeholder

--- app/PageNewsOne.js
+++ app/PageNewsOne.js
@@ -2,9 +2,34 @@ import React from 'react';
 import NavLink from "./NavLink";
 
 class PageNewsOne extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            newsList: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts/'+this.props.params.newsId)
+            .then((response) => response.json())
+            .then((news) => {
+                    this.setState({
+                        newsList: news
+                    });
+                })
+             .catch(alert);
+    }
     render() {
+        console.log(this.props.params);
+        console.log(this.state.newsList);
         return (
-            <div></div>
+            <div className="wrapper">
+                <NavLink to="news" className="link-news">All news</NavLink>
+                <div className="one-news">
+                    <h2>{this.state.newsList.title}</h2>
+                    <p>{this.state.newsList.body}</p>
+                </div>
+            </div>
         )
     }
 }
